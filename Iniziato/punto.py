5764fead6308192939e2abc8da94080b61b567de
ionchi <ion.chiriac@live.com>
2018-06-19 15:15:47 +0200

    vers. 2

--- Iniziato/punto.py
+++
@@ -1,6 +0,0 @@
-class points:
-
-    def __init__(self,n1,n2):
-        self.n1=n1
-        self.n2=n2
-        
--- crowPowered.py
+++ crowPowered.py
@@ -1,116 +1,110 @@
-#Item
 import random
 
-def p_n1n2_if_true(n1, n2, lie):
-    return ((1 - lie) ** n1) * (lie ** n2)
+M1 = 10
+M2 = 10
+TotalItems=2000
+K = 15
+Y0 = 59
+errorRate = 0.35
+selectivity = 0.2
+
+def p_n1n2_if_true(n1, n2, errorRate):
+    return ((1 - errorRate) ** n1) * (errorRate ** n2)
 
 
-def p_n1n2_if_false(n1, n2, lie):
-    return (lie ** n1) * ((1 - lie) ** n2)
+def p_n1n2_if_false(n1, n2, errorRate):
+    return (errorRate ** n1) * ((1 - errorRate) ** n2)
 
 
-def p_n1n2(n1, n2, true_a_priori, lie):
-    return true_a_priori * p_n1n2_if_true(n1, n2, lie) + (1 - true_a_priori) * p_n1n2_if_false(n1, n2, lie)
+def p_n1n2(n1, n2, true_a_priori, errorRate):
+    return true_a_priori * p_n1n2_if_true(n1, n2, errorRate) + (1 - true_a_priori) * p_n1n2_if_false(n1, n2, errorRate)
+
 
 #la probabilità effettivamente è uno
-def Pr1n1n2(n1, n2, true_a_priori, lie):
-    assert p_n1n2(n1, n2, true_a_priori, lie) != 0, 'Probability is not defined if P(n1, n2) = 0'
-
-    return true_a_priori * p_n1n2_if_true(n1, n2, lie) / p_n1n2(n1, n2, true_a_priori, lie)
-
-
-def Pr0n1n2(n1, n2, true_a_priori, lie):
-
-    return 1 - Pr1n1n2(n1, n2, true_a_priori, lie)
+def pr1n1n2(n1, n2, true_a_priori, errorRate):
+    assert p_n1n2(n1, n2, true_a_priori, errorRate) != 0, 'Probability is not defined if P(n1, n2) = 0'
+    return true_a_priori * p_n1n2_if_true(n1, n2, errorRate) / p_n1n2(n1, n2, true_a_priori, errorRate)
 
 
-def p0(n1, n2, a_priori, lie):
+def pr0n1n2(n1, n2, true_a_priori, errorRate):
+    return 1 - pr1n1n2(n1, n2, true_a_priori, errorRate)
+
+
+def p0(n1, n2, a_priori, errorRate):
     """
     the probability to observe a No if (n1, n2).
-
     we may estimate the probability to I=true, given (n1, n2).
     Knowing that I=true, the probability to get a "no" is the probability
     that the worker lie. (same for I=false)
-
     """
-    return Pr1n1n2(n1, n2, a_priori, lie) * lie + Pr0n1n2(n1, n2, a_priori, lie) * (1 - lie)
+    return pr1n1n2(n1, n2, a_priori, errorRate) * errorRate + pr0n1n2(n1, n2, a_priori, errorRate) * (1 - errorRate)
 
 
-def p1(n1, n2, a_priori, lie):
-    return Pr1n1n2(n1, n2, a_priori, lie) * (1 - lie) + Pr0n1n2(n1, n2, a_priori, lie) * lie
+def p1(n1, n2, a_priori, errorRate):
+    return pr1n1n2(n1, n2, a_priori, errorRate) * (1 - errorRate) + pr0n1n2(n1, n2, a_priori, errorRate) * errorRate
 
 
 def majority(n1, n2, M):
-  if n1 == ((M + 1) / 2):
-    return 1
-  if n2 == ((M + 1) / 2):
-    return 0
-  return 2
-
-def rectangular(n1, n2, M1=30, M2=30):
-  if n1 == M1:
-    return 1
-  if n2 == M2:
-    return 0
-  return 2
+    if n1 == ((M + 1) / 2):
+        return 1
+    if n2 == ((M + 1) / 2):
+        return 0
+    return 2
 
 
+def rectangular(n1, n2):
+    if n1 == M1:
+        return 1
+    if n2 == M2:
+        return 0
+    return 2
 
 
-
-def Y(n1, n2, Y0, lie, a_priori,strategy=rectangular):
-    if(n1==n2==0):
+def Y(n1, n2, Y0, errorRate, a_priori, strategy=rectangular):
+    if n1 == n2 == 0:
         return Y0
     if strategy(n1, n2) == 0:
         return Y0
-
     if strategy(n1, n2) == 1:
-        return 0;
-
+        return 0
     return min(Y0,
-               1 + p1(n1, n2, a_priori, lie) * Y(n1 + 1, n2, Y0,  lie=lie, a_priori=a_priori,strategy=strategy)
-               + p0(n1, n2, a_priori, lie) * Y(n1, n2 + 1, Y0, lie=lie, a_priori=a_priori, strategy=strategy))
+               1 + p1(n1, n2, a_priori, errorRate) * Y(n1 + 1, n2, Y0, errorRate=errorRate, a_priori=a_priori, strategy=strategy)
+               + p0(n1, n2, a_priori, errorRate) * Y(n1, n2 + 1, Y0, errorRate=errorRate, a_priori=a_priori, strategy=strategy))
 
 
-def inizializzazione(nItem,Selettivita):
-    import random
-    #nItem=100
-    #Selettivita=0.2
+def inizializzazione(TotalItems, selectivity):
     Item=[]
     i=0
-    while(i<=nItem):
-        while(i<=nItem*Selettivita):
+    while(i <= TotalItems):
+        while(i <= TotalItems * selectivity):
             Item.append(1)
             i+=1
         Item.append(0)
         i+=1
     random.shuffle(Item)
-    #print(Item)
     Strategy={}
     c=0
-    while(c<nItem):
+    while(c < TotalItems):
         Strategy[c]=[(0,0)]
         c+=1
     #print(Strategy)
-    return Item,Strategy
+    return Item, Strategy
+
 
 def min2(Y,n,Strategy):
 
-    app={}
     app=Strategy.copy()
     lower=[]
-    while(len(lower)<n):
-
+    while len(lower) < n:
         low=99999
         for key in app:
-
             if (Y[app[key][0]]<low):
                 low=Y[app[key][0]]
                 chiave=key
         lower.append(chiave)
         del app[chiave]
-        low=99999
     return lower
+
 
 def Y00():
     alfa=150
@@ -135,31 +129,26 @@ def Y00():
 #Y00()
 
 
-K=15
-Y0=59
-lie=0.1
-selettivita=0.2
-M1=30
-M2=30
-Item,Strategy=inizializzazione(200,selettivita)
-#print(Strategy)
+# Inizio MAIN
+
+Item, Strategy = inizializzazione(TotalItems, selectivity)
 
 strategy2=Strategy.copy()
 l=[]
 u=[]
 y={}
-#for i in Item:
-  #  u.append(i)
+
 u=Item.copy()
 app=[]
-#aggiorno elenenco delle y note con i nuovi punti della strategia
+
+#aggiorno elenco delle y note con i nuovi punti della strategia
 for key in Strategy:
     if(not Strategy[key] in app):
         app.append(Strategy[key])
-for ap in app:
 
+for ap in app:
     if(not ap[0] in y):
-        y[ap[0]]=Y(ap[0][0],ap[0][1],Y0,lie,selettivita)
+        y[ap[0]]=Y(ap[0][0], ap[0][1], Y0, errorRate, selectivity)
 
 fasi=0
 domande=0
@@ -167,24 +156,25 @@ while(len(l)<K):
     fasi+=1
     I2=min2(y,K-len(l),Strategy)
     cq={}
-    for i in I2:
 
-        n1=M1-Strategy[i][0][1]
-        n2=M2-Strategy[i][0][0]
+    for i in I2:
+        n1=M1-Strategy[i][0][0]
+        n2=M2-Strategy[i][0][1]
         cq[i] =min(n1,n2)
         domande+=cq[i]
+
     for i in cq:
         c=0
         while(c<cq[i]):
             #simulo crowdsourcing
-            if(random.random()>lie):
-                    if(Item[i]==1):
-                        a=Strategy[i][0][0]+1
-                        b=Strategy[i][0][1]
-                    else:
-                        a = Strategy[i][0][0]
-                        b = Strategy[i][0][1]+1
-                    Strategy[i]=[(a,b)]
+            if(random.random()>errorRate):
+                if(Item[i]==1):
+                    a=Strategy[i][0][0]+1
+                    b=Strategy[i][0][1]
+                else:
+                    a = Strategy[i][0][0]
+                    b = Strategy[i][0][1]+1
+                Strategy[i]=[(a,b)]
             else:
                 if (Item[i] == 1):
                     a = Strategy[i][0][0]
@@ -203,17 +193,16 @@ while(len(l)<K):
                 del(Strategy[i])
             else:
                 if not Strategy[i][0] in y:
-                    y[Strategy[i][0]]=Y(Strategy[i][0][0],Strategy[i][0][1],Y0,lie,selettivita)
+                    y[Strategy[i][0]]=Y(Strategy[i][0][0], Strategy[i][0][1], Y0, errorRate, selectivity)
 
+print("oggetti")
+uni=0
 for p in l:
+    if(Item[p]==1):
+        uni+=1
     print(p,Item[p])
+
+print(uni/len(l))
 print("risultato ottenuto in:\n"+"\tfasi: "+str(fasi)+"\n"+"\tdomande:"+str(domande) )
 
-
-
-
-
-
-
-
-
+# Fine MAIN
